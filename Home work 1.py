ecf4b5a5c5f444d9032c523f5fdbbf01d47108dd
hiba zalloum <hebahzalloum@gmail.com>
2020-10-11 18:59:11 +0300

    secound

--- Home work 1.py
+++ Home work 1.py
@@ -10,24 +10,53 @@ def clean(data, irrelevant):
     return data
 
 
+def search(X1, word):
+    ans = 0
+    ans_val = []
+
+    for i in X1:
+        if word in i:
+            ans = ans + 1
+            ans_val.append(i)
+
+    return ans, ans_val
+
+
 irrelevant_words = {".", ',', '"\"', ";", ":", " "}
 cleanData = clean(movie, irrelevant_words)
 
+
 document = cleanData.splitlines()
+
+
+def split_line(document):
+    document = document.split()
+    for word in document:
+        print(document)
+
+document = split_line(document)
+#for terms in document:
+    #term = terms.split()
+
+
 # print(docId[0])
 # print(docId)
 
-for docId in range(len(document)):
-     #print(document, end=str(docId))
-    print(" ")
+#for docId in range(len(document)):
+    #print(document, end=str(docId))
+    #print(" ")
+
+word = input("Enter your search : ")
+# Assume this is a document
+
+# print(data, '\n', word)
+
+No_of_result, result_val = search(document, word)
+
+for x in result_val:
+    print('\n', '================================== New result =================================', '\n')
+    print(x)
+#
+print('No_of_result = ', No_of_result)
 
 
-def intersect(p1, p2):
-    ans = 0
-    ans_val = []
-    for i in p1:
-        for j in p2:
-            if i == j:
-                ans = ans + 1
-                ans_val.append(i)
-    return ans, ans_val
